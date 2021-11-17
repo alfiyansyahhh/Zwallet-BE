02ebb4b2999c8257a4a18c10954e6877419bbca2
alfiyansyah <65682948+alfiyansyahhh@users.noreply.github.com>
2021-11-18 03:02:52 +0700

    fixed env

--- src/helpers/env.js
+++ src/helpers/env.js
@@ -4,5 +4,7 @@ const env = {
     DB_USERNAME : process.env.DB_USERNAME,
     DB_PASSWORD : process.env.DB_PASSWORD,
     JWT_SECRET  : process.env.JWT_SECRET,
+    DB_NAME     : process.env.DB_NAME,
+    DB_HOST     : process.env.DB_HOST
 };
 module.exports = env
